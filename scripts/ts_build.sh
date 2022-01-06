3c2b4cf4657adc42d5d65d9dd9152e9e2a31feb3
Hui Yang <hyang027a@gmail.com>
2022-01-05 22:47:15 -0800

    manual build

--- scripts/ts_build.sh
+++ scripts/ts_build.sh
@@ -8,21 +8,21 @@ cd ${BASEDIR}/../
 PROTO_DEST=./build/proto
 
 mkdir -p ${PROTO_DEST}
-echo $PWD 
+cd src/v1/
+echo $PWD
 # JavaScript code generation
-yarn run grpc_tools_node_protoc \
---ts_out=generate_package_definition:${PROTO_DEST} \
---js_out=import_style=commonjs,binary:${PROTO_DEST} \
---grpc_out=generate_package_definition:${PROTO_DEST} \
--I ./src \
--I src/v1/*.proto
+# /Users/apple/Desktop/1215/testNode/node_modules/.bin/grpc_tools_node_protoc --js_out=import_style=commonjs,binary:. --grpc_out=generate_package_definition:. helloworld.proto
 
-# Strip out eval statements for CSP reasons (avoids requiring `unsafe-eval`)
-# See: https://github.com/protocolbuffers/protobuf/blob/fae6773539b40b4904f2bfa8e09e69697984a65f/src/google/protobuf/compiler/js/js_generator.cc#L3644
-find ${PROTO_DEST} \
--path ${PROTO_DEST}/node_modules -prune -false -o \
--name "*.js" \
--exec sed -i -e "s/var global = Function('return this')();/var global = globalThis\;/g" {} \;
+# # move not proto files away
+# find ./ -maxdepth 1 -type f ! -name '*.proto' -exec mv {} ../../build/proto \;
 
-# Prepare the output directory for publishing
-prettier ${PROTO_DEST} --write
+# cd ../../
+# # Strip out eval statements for CSP reasons (avoids requiring `unsafe-eval`)
+# # See: https://github.com/protocolbuffers/protobuf/blob/fae6773539b40b4904f2bfa8e09e69697984a65f/src/google/protobuf/compiler/js/js_generator.cc#L3644
+# find ${PROTO_DEST} \
+# -path ${PROTO_DEST}/node_modules -prune -false -o \
+# -name "*.js" \
+# -exec sed -i -e "s/var global = Function('return this')();/var global = globalThis\;/g" {} \;
+
+# # Prepare the output directory for publishing
+# prettier ${PROTO_DEST} --write
