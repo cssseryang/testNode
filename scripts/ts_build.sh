338449cb3ec84e9b347704ed7646fb4a66f9c5f5
Hui Yang <hyang027a@gmail.com>
2022-01-05 22:23:26 -0800

    test on GHA

--- scripts/ts_build.sh
+++ scripts/ts_build.sh
@@ -15,7 +15,7 @@ yarn run grpc_tools_node_protoc \
 --js_out=import_style=commonjs,binary:${PROTO_DEST} \
 --grpc_out=generate_package_definition:${PROTO_DEST} \
 -I ./src \
--I ./src/v1/*.proto
+-I src/v1/*.proto
 
 # Strip out eval statements for CSP reasons (avoids requiring `unsafe-eval`)
 # See: https://github.com/protocolbuffers/protobuf/blob/fae6773539b40b4904f2bfa8e09e69697984a65f/src/google/protobuf/compiler/js/js_generator.cc#L3644
